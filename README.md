72f3a16a2c92d172079fc00985bd59c8264b2bd2
Augustas Budnikas <budnikiukas@gmail.com>
2024-09-09 22:55:28 +0300

    Task 1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# fp-2024
+# FP-2024 | Vehicle Garage
+
+### To get started, you first need to open the project using Visual Studio Code and having Docker Desktop
+1. `Ctrl + Shift + P`
+2. `Dev Containers: Open Folder in Container`
+
+### To Build & Test the Project, run the following commands
+1. `stack build`
+2. `stack test`
+
+### To Execute a Lecture Example
+1. `stack ghci`
+2. `:l src/Lessons/Lesson01.hs`
--- src/Lib1.hs
+++ src/Lib1.hs
@@ -1,8 +1,30 @@
 module Lib1
-    ( completions
-    ) where
+  ( completions,
+  )
+where
 
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions =
+  [ -- Actions
+    "add_vehicle",
+    "perform_maintenance",
+    "sell_vehicle",
+    "inventory",
+    -- Vehicle Types
+    "Car",
+    "Truck",
+    "Motorcycle",
+    "SUV",
+    -- Maintenance Types
+    "OilChange",
+    "TireRotation",
+    "BrakeInspection",
+    "EngineTuneUp",
+    -- Mileage
+    "km",
+    -- Duration
+    "hours",
+    "days"
+  ]
