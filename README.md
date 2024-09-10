5c20755301ff97c4104052b376bb8e6825e4b79f
Labžentis Eimantas <eimantas.labzentis@mif.stud.vu.lt>
2024-09-10 06:50:10 +0000

    Environment setup and test passing

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# fp-2024
+# fp-2024 | LIBRARY SYSTEM
 
-test
+
+**Open the Project in a Dev Container:**
+> Command + Shift + P (MacOS) \
+> Dev Containers: Open Folder in Container
+
+
+**Build and Test**
+> stack build \
+> stack test
+
+
+**Try Lecture Code**
+> ghci src/Lessons/Lesson01.hs
--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,39 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions = [
+    -- Entities
+    "book",
+    "title",
+    "author",
+    "genre",
+    "isbn",
+    "publisher",
+    "publication-date",
+    "borrower",
+    "due-date",
+    "library",
+    "shelf",
+    "category",
+
+    -- Genres
+    "Fiction",
+    "Non-Fiction",
+    "Mystery",
+    "Fantasy",
+    "Science Fiction",
+    "Biography",
+    "Historical",
+    "Thriller",
+
+    -- Commands
+    "addBook",
+    "removeBook",
+    "borrowBook",
+    "returnBook",
+    "searchBook",
+    "listAvailableBooks",
+    "listBorrowedBooks",
+    "checkDueDate"
+    ]
+
