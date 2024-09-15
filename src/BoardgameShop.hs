75d72e87054925b1598bb6c88046943f7e4e89dd
Erelix <adrian.klimasevski@gmail.com>
2024-09-15 15:23:27 +0000

    Added comments

--- src/BoardgameShop.hs
+++ src/BoardgameShop.hs
@@ -1,35 +1,50 @@
-module BoardgameShop (catanBase) where
+module BoardgameShop (boargameShop) where
 
--- Information
+boargameShop :: String
+boargameShop = "Hello and welcome to the boardgame shop!"
+
+-- <<Information>>
 bundleDiscount :: Double
 bundleDiscount = 15
 
 shippingCost :: Double
 shippingCost = 3.99
---
+-- <<>>
 
--- Primary functions
+-- <<Primary functions>>
+
 add :: Double -> Double -> Double
-add a b = a + b    
+add a b = a + b   
+-- | This function returns the sum of 
+-- two given Doubles
 
 checkShipping :: Double -> Double 
 checkShipping a =
     if a >= 70 
         then a
     else add a shippingCost
+-- | This function checks if the 
+-- shipping cost should be ignored
 
 buy :: Double -> Double
 buy a = roundTo 2 (checkShipping a )
+-- | This function buys the given product
 
 giveDiscountForBundle :: Double -> Double
 giveDiscountForBundle a = a * ((100 - bundleDiscount) / 100)
+-- | This function buys the calculates
+-- a discout for the bundle
+
 
 roundTo :: Int -> Double -> Double
 roundTo n x = (fromInteger $ round $ x * (10^n)) / (10.0^^n)
 -- from https://www.tutorialspoint.com/haskell-program-to-round-a-number-to-n-decimal-places
---
+-- | This rounds up to n decimal spaces
+
+-- <<>>
 
 
+-- <<Products>>
 
 -- Base game prices
 catanBase :: Double
@@ -50,9 +65,12 @@ catanExp_citiesandknights :: Double
 catanExp_citiesandknights = 32.50
 --
 
--- Special bundle
+-- <<>>
+
+-- <<Special offers>>
 catanWithAllExpBundle :: Double
 catanWithAllExpBundle = giveDiscountForBundle (add catanBase (add catanExp_5to6players (add catanExp_seafarers catanExp_citiesandknights)))
+-- <<>>
 
 
 
