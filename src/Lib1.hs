d999be541249b3b8a063cc1c34ece81defff2648
Aleksej Krasavcev <aleksej.krasavcev@mif.stud.vu.lt>
2024-09-17 20:41:44 +0000

    added commands

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,4 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions = ["plant", "cut", "inspect"]
