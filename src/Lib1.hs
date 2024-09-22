6d8e4b0fe15e7f81c8a14ba83e0ba16143547470
Nikas Zabukas <nikas.zabukas@mif.stud.vu.lt>
2024-09-22 19:26:32 +0000

    1 Lab job

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,50 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions =
+  [ -- Actions
+    "add_item",
+    "delete_item",
+    "restock_item",
+    "sell_item",
+    "inventory",
+    -- Food Items
+    "Fruits",
+    "Vegetables",
+    "Grains",
+    "Dairy",
+    "Meats",
+    -- Fruit Types
+    "Apples",
+    "Bananas",
+    "Oranges",
+    -- Vegetable Types
+    "Carrots",
+    "Potatoes",
+    "Spinach",
+    -- Grain Types
+    "Rice",
+    "Bread",
+    "Pasta",
+    -- Dairy Types
+    "Milk",
+    "Cheese",
+    "Yogurt",
+    -- Meat Types
+    "Chicken",
+    "Beef",
+    "Fish",
+    -- Beverages
+    "Soda",
+    "Juice",
+    "Water",
+    -- Household Supplies
+    "CleaningProducts",
+    "PaperGoods",
+    -- Cleaning Products Types
+    "Detergent",
+    "Soap",
+    -- Paper Goods Types
+    "PaperTowels",
+    "ToiletPaper"
+  ]
