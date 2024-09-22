b5c4c061ced37adfeb72108c103ab888cc83dcb3
Paulius Navikas <paulius.navikas@mif.stud.vu.lt>
2024-09-22 23:21:47 +0300

    First task initial completion

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -2,7 +2,31 @@ module Lib1
     ( completions
     ) where
 
+
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+
+completions =
+  [
+	-- Art piece information
+	"type",
+	"title",
+	"artist",
+	"date",
+	"price", 
+	"description",
+	-- Art piece types
+	"Painting",
+	"Photograph", 
+	"Sculpture",
+	"Digital",
+	"Drawing", 
+	"Sketch",
+	-- Price
+	"€",
+	"$",
+	-- Name
+	"first_name",
+	"last_name"
+  ]
