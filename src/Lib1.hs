1d9ebebc3733d31e937ac630292bdf8b411e898b
Andrius Šilinskas <andrius.silinskas@mif.stud.vu.lt>
2024-09-30 09:12:22 +0300

    bnf failo sukurimas ir llib1 papildymas

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,4 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions = ["toy", "simple_toy", "composite_toy", "toy_list", "decorated_toy", "decoration", "color", "size", "material"]
