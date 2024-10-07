44bc68301b62e105e0b0ae0ae661d52cc7341a8b
Nikita Savickis <nikita.savickis@mif.stud.vu.lt>
2024-10-07 17:22:43 +0000

    added first laboratory work

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -2,7 +2,13 @@ module Lib1
     ( completions
     ) where
 
--- | This function returns a list of words
--- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+    , "(", ")", "{", "}", "[", ""
+    , "Week:"
+    , "x"
+    , "Superset of ["
+    , "repetitions of"
+    , "leg-extensions", "squats", "pull-ups", "bench-press", "deadlift"
+    , "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"  
+    ]
