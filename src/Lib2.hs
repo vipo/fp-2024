9f7c556869532a0bee8781753b959369ce0f4a0f
Augustas Budnikas <budnikiukas@gmail.com>
2024-10-11 21:20:05 +0300

    Own Parser

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -10,7 +10,7 @@ module Lib2
   )
 where
 
-import Text.Read (readMaybe)
+import Data.Char (isDigit)
 
 -- | Data types for Queries and State
 data Query
@@ -39,57 +39,160 @@ data State = State
 emptyState :: State
 emptyState = State {vehicles = [], inventory = []}
 
+-- | Basic parsers
+type Parser a = String -> Either String (a, String)
+
+skipSpaces :: String -> String
+skipSpaces = dropWhile (== ' ')
+
+parseChar :: Char -> Parser Char
+parseChar _ [] = Left "Unexpected end of input"
+parseChar c input =
+  let input' = skipSpaces input
+  in if null input'
+     then Left "Unexpected end of input"
+     else if head input' == c
+          then Right (c, tail input')
+          else Left $ "Expected '" ++ [c] ++ "', but found '" ++ [head input'] ++ "'"
+
+parseLiteral :: String -> Parser String
+parseLiteral [] input = Right ([], input)
+parseLiteral (x:xs) input =
+  let input' = skipSpaces input
+  in if null input'
+     then Left "Unexpected end of input"
+     else if head input' == x
+          then case parseLiteral xs (tail input') of
+                 Right (str, rest) -> Right (x:str, rest)
+                 Left err -> Left err
+          else Left $ "Expected \"" ++ (x:xs) ++ "\", but found \"" ++ take (length (x:xs)) input' ++ "\""
+
+parseString :: Parser String
+parseString input =
+  let input' = skipSpaces input
+  in if null input'
+     then Right ("", "")
+     else if head input' == '"'
+          then parseQuotedString (tail input')
+          else let (str, rest) = span (\c -> c /= ' ' && c /= ',' && c /= '(' && c /= ')') input'
+               in Right (str, rest)
+  where
+    parseQuotedString [] = Left "Unexpected end of input in quoted string"
+    parseQuotedString ('"':rest) = Right ("", rest)
+    parseQuotedString (x:rest) = case parseQuotedString rest of
+      Right (str, rest') -> Right (x:str, rest')
+      Left err -> Left err
+
+parseInt :: Parser Int
+parseInt input =
+  let (digits, rest) = span isDigit (skipSpaces input)
+  in if null digits
+     then Left "Expected an integer"
+     else Right (read digits, rest)
+
+parseDouble :: Parser Double
+parseDouble input =
+  let (digits, rest) = span (\c -> isDigit c || c == '.') (skipSpaces input)
+  in if null digits
+     then Left "Expected a double"
+     else Right (read digits, rest)
+
 -- | Parse vehicle type
-parseVehicleType :: String -> Maybe VehicleType
-parseVehicleType "Car" = Just Car
-parseVehicleType "Truck" = Just Truck
-parseVehicleType "Motorcycle" = Just Motorcycle
-parseVehicleType "SUV" = Just SUV
-parseVehicleType _ = Nothing
+parseVehicleType :: Parser VehicleType
+parseVehicleType input = case parseString (skipSpaces input) of
+  Right ("Car", rest) -> Right (Car, rest)
+  Right ("Truck", rest) -> Right (Truck, rest)
+  Right ("Motorcycle", rest) -> Right (Motorcycle, rest)
+  Right ("SUV", rest) -> Right (SUV, rest)
+  _ -> Left "Failed to parse vehicle type"
 
 -- | Parse maintenance type
-parseMaintenanceType :: String -> Maybe MaintenanceType
-parseMaintenanceType "OilChange" = Just OilChange
-parseMaintenanceType "TireRotation" = Just TireRotation
-parseMaintenanceType "BrakeInspection" = Just BrakeInspection
-parseMaintenanceType "EngineTuneUp" = Just EngineTuneUp
-parseMaintenanceType _ = Nothing
+parseMaintenanceType :: Parser MaintenanceType
+parseMaintenanceType input = case parseString (skipSpaces input) of
+  Right ("OilChange", rest) -> Right (OilChange, rest)
+  Right ("TireRotation", rest) -> Right (TireRotation, rest)
+  Right ("BrakeInspection", rest) -> Right (BrakeInspection, rest)
+  Right ("EngineTuneUp", rest) -> Right (EngineTuneUp, rest)
+  _ -> Left "Failed to parse maintenance type"
 
--- | Parses user's input into a Query.
+-- | Parse duration unit
+parseDurationUnit :: Parser (Int -> Duration)
+parseDurationUnit input = case parseString (skipSpaces input) of
+  Right ("days", rest) -> Right (Days, rest)
+  Right ("hours", rest) -> Right (Hours, rest)
+  _ -> Left "Failed to parse duration unit"
+
+-- | Parse duration
+parseDuration :: Parser Duration
+parseDuration input = case parseInt input of
+  Right (num, rest) -> case parseDurationUnit rest of
+    Right (unit, rest') -> Right (unit num, rest')
+    Left err -> Left err
+  Left err -> Left err
+
+-- | Parse add vehicle query
+parseAddVehicle :: Parser Query
+parseAddVehicle input = do
+  (_, rest) <- parseLiteral "add_vehicle" input
+  (_, rest1) <- parseChar '(' rest
+  (vType, rest2) <- parseVehicleType rest1
+  (_, rest3) <- parseChar ',' rest2
+  (model, rest4) <- parseString rest3
+  (_, rest5) <- parseChar ',' rest4
+  (year, rest6) <- parseInt rest5
+  (_, rest7) <- parseChar ',' rest6
+  (mileage, rest8) <- parseInt rest7
+  (_, rest9) <- parseChar ')' rest8
+  return (AddVehicle vType model year mileage, rest9)
+
+-- | Parse perform maintenance query
+parsePerformMaintenance :: Parser Query
+parsePerformMaintenance input = do
+  (_, rest) <- parseLiteral "perform_maintenance" input
+  (_, rest1) <- parseChar '(' rest
+  (vType, rest2) <- parseVehicleType rest1
+  (_, rest3) <- parseChar ',' rest2
+  (mType, rest4) <- parseMaintenanceType rest3
+  (_, rest5) <- parseChar ',' rest4
+  (duration, rest6) <- parseDuration rest5
+  (_, rest7) <- parseChar ')' rest6
+  return (PerformMaintenance vType mType duration, rest7)
+
+-- | Parse sell vehicle query
+parseSellVehicle :: Parser Query
+parseSellVehicle input = do
+  (_, rest) <- parseLiteral "sell_vehicle" input
+  (_, rest1) <- parseChar '(' rest
+  (vType, rest2) <- parseVehicleType rest1
+  (_, rest3) <- parseChar ',' rest2
+  (model, rest4) <- parseString rest3
+  (_, rest5) <- parseChar ',' rest4
+  (year, rest6) <- parseInt rest5
+  (_, rest7) <- parseChar ',' rest6
+  (price, rest8) <- parseDouble rest7
+  (_, rest9) <- parseChar ')' rest8
+  return (SellVehicle vType model year price, rest9)
+
+-- | Parse inventory query
+parseInventory :: Parser Query
+parseInventory input = do
+  (_, rest) <- parseLiteral "inventory" input
+  (_, rest1) <- parseChar '(' rest
+  (vType, rest2) <- parseVehicleType rest1
+  (_, rest3) <- parseChar ')' rest2
+  return (Inventory vType, rest3)
+
+-- | Combine all query parsers
 parseQuery :: String -> Either String Query
-parseQuery input =
-  let sanitizedInput = words (map (\c -> if (c == ',') || (c `elem` "()\"") then ' ' else c) input)
-   in case sanitizedInput of
-        -- <add_vehicle> ::= "add_vehicle" "(" <vehicle_type> "," <model> "," <year> "," <mileage> ")"
-        ("add_vehicle" : v : model : year : mileage : _) ->
-          case (parseVehicleType v, readMaybe year :: Maybe Int, readMaybe mileage :: Maybe Int) of
-            (Just vehicleType, Just y, Just m) -> Right (AddVehicle vehicleType model y m)
-            _ -> Left "Failed to parse add_vehicle"
-        -- <perform_maintenance> ::= "perform_maintenance" "(" <vehicle_type> "," <maintenance_type> "," <duration_number> "," <duration_unit> ")"
-        ("perform_maintenance" : v : m : durationNum : durationUnit : _) ->
-          case (parseVehicleType v, parseMaintenanceType m, readMaybe durationNum :: Maybe Int, parseDurationUnit durationUnit) of
-            (Just vehicleType, Just maintenanceType, Just num, Just dur) ->
-              Right (PerformMaintenance vehicleType maintenanceType (dur num))
-            _ -> Left "Failed to parse perform_maintenance"
-        -- <sell_vehicle> ::= "sell_vehicle" "(" <vehicle_type> "," <model> "," <year> "," <price> ")"
-        ("sell_vehicle" : v : model : year : price : _) ->
-          case (parseVehicleType v, readMaybe year :: Maybe Int, readMaybe price :: Maybe Double) of
-            (Just vehicleType, Just y, Just p) -> Right (SellVehicle vehicleType model y p)
-            _ -> Left "Failed to parse sell_vehicle"
-        -- <inventory> ::= "inventory" "(" <vehicle_type> ")"
-        ("inventory" : v : _) ->
-          case parseVehicleType v of
-            Just vehicleType -> Right (Inventory vehicleType)
-            _ -> Left "Failed to parse inventory"
-        _ -> Left "Unknown command"
-
--- | Parse duration unit from a string like "days" or "hours".
-parseDurationUnit :: String -> Maybe (Int -> Duration)
-parseDurationUnit unit =
-  case unit of
-    "days"  -> Just Days
-    "hours" -> Just Hours
-    _       -> Nothing
+parseQuery input = case parseAddVehicle input of
+  Right (query, _) -> Right query
+  Left err1 -> case parsePerformMaintenance input of
+    Right (query, _) -> Right query
+    Left err2 -> case parseSellVehicle input of
+      Right (query, _) -> Right query
+      Left err3 -> case parseInventory input of
+        Right (query, _) -> Right query
+        Left err4 -> Left $ "Failed to parse query: " ++ err1 ++ "; " ++ err2 ++ "; " ++ err3 ++ "; " ++ err4
 
 -- | Transition the state with a new query.
 stateTransition :: State -> Query -> Either String (Maybe String, State)
