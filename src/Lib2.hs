f5188bc9c2d7c54fbbe3116e687546b1b5064cc5
Ramunė Riaubaitė <ramune.riaubaite@mif.stud.vu.lt>
2024-10-11 06:14:27 +0000

    Lab2 work

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,44 +1,154 @@
-{-# LANGUAGE InstanceSigs #-}
 module Lib2
-    ( Query(..),
-    parseQuery,
-    State(..),
-    emptyState,
-    stateTransition
+    ( Animal(..), 
+      Query(..),
+      parseAnimal,
+      parseString,
+      parseNumber,
+      parseQuery, 
+      parseCompoundQuery,
+      parseAdd,
+      parseDelete,
+      State(..),
+      emptyState,
+      stateTransition,
+      runREPL,
     ) where
 
--- | An entity which represets user input.
--- It should match the grammar from Laboratory work #1.
--- Currently it has no constructors but you can introduce
--- as many as needed.
+
+import qualified Data.Char as C
+import Data.List (isPrefixOf)
+
+-- 1) An entity which represents user input.
+data Animal = Animal { species :: String, name :: String, age :: Int }
+    deriving (Show, Eq)
+
 data Query
+    = Add Animal
+    | Delete Animal
+    | ListAnimals
+    | CompoundQuery Query Query  -- recursive constructor
+    deriving (Show, Eq)
 
--- | The instances are needed basically for tests
-instance Eq Query where
-  (==) _ _= False
+-- <animal> ::= <species> <name> <age>
+parseAnimal :: String -> Either String Animal
+parseAnimal s = 
+    parseString s >>= \(speciesV, rest1) ->
+    parseString (dropWhile (== ' ') rest1) >>= \(nameV, rest2) ->
+    parseNumber (dropWhile (== ' ') rest2) >>= \(ageV, _) ->
+    Right (Animal speciesV nameV ageV)
 
-instance Show Query where
-  show _ = ""
+-- <species> ::= <string>
+-- <name> ::= <string>
+parseString :: String -> Either String (String, String)
+parseString [] = Left "Unexpected end of input"
+parseString s = case span C.isAlpha s of
+    ("", _) -> Left "Expected string, but got none"
+    (str, rest) -> Right (str, rest)
 
--- | Parses user's input.
--- The function must have tests.
+-- <age> ::= <integer>
+parseNumber :: String -> Either String (Int, String)
+parseNumber s = case span C.isDigit s of
+    ("", _) -> Left "Expected number, but got none"
+    (numStr, rest) -> Right (read numStr, rest)
+
+-- <command> ::= <add_animal> | <delete_animal> | 'list_animals' | <compound_query>
 parseQuery :: String -> Either String Query
-parseQuery _ = Left "Not implemented 2"
+parseQuery s 
+    | null s = Left "Expected some command but did not get anything"
+    | "list_animals" `isPrefixOf` s = Right ListAnimals
+    | otherwise = parseCompoundQuery s `orElse` parseAdd s `orElse` parseDelete s
 
--- | An entity which represents your program's state.
--- Currently it has no constructors but you can introduce
--- as many as needed.
-data State
+-- <compound_query> ::= <command> ';' <command>
+parseCompoundQuery :: String -> Either String Query
+parseCompoundQuery s = 
+    case break (== ';') s of
+        (firstCmd, ';':restCmd) -> do
+            query1 <- parseQuery firstCmd
+            query2 <- parseQuery (dropWhile (== ' ') restCmd)
+            Right (CompoundQuery query1 query2)
+        _ -> Left "Expected a compound query, but got a single query."
 
--- | Creates an initial program's state.
--- It is called once when the program starts.
+-- <add_animal> ::= 'ADD' <animal>
+parseAdd :: String -> Either String Query
+parseAdd s = do
+    rest1 <- parseLiteral "ADD " s
+    animal <- parseAnimal (dropWhile (== ' ') rest1)
+    return (Add animal)
+
+-- <delete_animal> ::= 'DELETE' <species> <name> <age>
+parseDelete :: String -> Either String Query
+parseDelete s = do
+    rest1 <- parseLiteral "DELETE " s
+    animal <- parseAnimal (dropWhile (== ' ') rest1)
+    return (Delete animal)
+
+-- Checks if the input starts with the given literal (needed for parseAdd and parseDelete)
+parseLiteral :: String -> String -> Either String String
+parseLiteral literal s 
+    | literal `isPrefixOf` s = Right (drop (length literal) s)
+    | otherwise = Left ("Did not get a valid command")
+
+-- 4) An entity which represents your program's state.
+data State = State [Animal]
+    deriving (Show)
+
+-- 5) Creates an initial program's state.
 emptyState :: State
-emptyState = error "Not implemented 1"
+emptyState = State []
 
--- | Updates a state according to a query.
--- This allows your program to share the state
--- between repl iterations.
--- Right contains an optional message to print and
--- an updated program's state.
+-- 6) Updates a state according to a query.
 stateTransition :: State -> Query -> Either String (Maybe String, State)
-stateTransition _ _ = Left "Not implemented 3"
+stateTransition (State animals) (Add animal) =
+    if animal `elem` animals
+    then Left ("Animal " ++ show animal ++ " already exists.")
+    else Right (Just ("Added animal: " ++ show animal), State (animal : animals))
+
+stateTransition (State animals) (Delete animal) =
+    if animal `elem` animals
+    then Right (Just ("Deleted animal: " ++ show animal), State (filter (/= animal) animals))
+    else Left ("Animal " ++ show animal ++ " not found.")
+
+stateTransition (State animals) ListAnimals =
+    if null animals
+    then Right (Just "No animals found.", State animals)
+    else Right (Just ("Current animals: " ++ show animals), State animals)
+
+-- Handles compound queries
+stateTransition state (CompoundQuery q1 q2) = do
+    (msg1, newState) <- stateTransition state q1
+    (msg2, finalState) <- stateTransition newState q2
+    let combinedMsg = unwords $ filter (not . null) [maybe "" id msg1, maybe "" id msg2]
+    Right (Just combinedMsg, finalState)
+
+-- Helper to try one parser or another
+orElse :: Either String a -> Either String a -> Either String a
+orElse (Left _) y = y
+orElse x _ = x
+
+-- for testing via terminal
+runREPL :: State -> IO ()
+runREPL state = do
+    putStr ">>> "
+    command <- getLine
+    case command of
+        "exit" -> putStrLn "Exiting the program."
+        _ -> do
+            case parseQuery command of
+                Left err -> putStrLn ("Error: " ++ err)
+                Right query -> do
+                    -- state transition based on the parsed query
+                    case stateTransition state query of
+                        Left err -> putStrLn ("Error: " ++ err)
+                        Right (msg, newState) -> do
+                            putStrLn (maybe "" id msg)
+                            runREPL newState  -- continue the loop with the updated state
+
+-- main :: IO ()
+-- main = do
+--   print (parseAnimal "giraffe Lin 10") -- output: Right (Animal {species = "giraffe", name = "Lin", age = 10})
+--   print (parseString "HereLives5Animals") -- output: Right ("HereLives","5Animals")
+--   print (parseNumber "30Dogs") -- output: Right (30,"Dogs")
+--   print (parseQuery "ADD cat Mino 4") -- output: Right (Add (Animal {species = "cat", name = "Mino", age = 4}))
+--   print (parseCompoundQuery "ADD hamster Lili 1; DELETE hamster Lili 1") -- output: Right (CompoundQuery (Add (Animal {species = "hamster", name = "Lili", age = 1})) (Delete (Animal {species = "hamster", name = "Lili", age = 1})))
+--   print (parseAdd "ADD fish Jojo 1") -- output: Right (Add (Animal {species = "fish", name = "Jojo", age = 1}))
+--   print (parseDelete "DELETE cat Mo 4") -- output: Right (Delete (Animal {species = "cat", name = "Mo", age = 4}))
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 import Test.Tasty ( TestTree, defaultMain, testGroup )
 import Test.Tasty.HUnit ( testCase, (@?=) )
-
-import Lib1 qualified
 import Lib2 qualified
 
 main :: IO ()
@@ -12,11 +10,162 @@ tests :: TestTree
 tests = testGroup "Tests" [unitTests]
 
 unitTests :: TestTree
-unitTests = testGroup "Lib1 tests"
-  [ testCase "List of completions is not empty" $
-      null Lib1.completions @?= False,
-    testCase "Parsing case 1 - give a better name" $
-      Lib2.parseQuery "" @?= (Left "Some error message"),
-    testCase "Parsing case 2 - give a better name" $
-      Lib2.parseQuery "o" @?= (Left "Some error message")
+unitTests = testGroup "Lib2 tests"
+  [
+
+    -- No need for 'Animal' tests bc it is just data type
+
+    -- No need for 'Query' tests bc it is just data type
+
+
+    -- for parseAnimal
+    testCase "parseAnimal valid input" $
+      Lib2.parseAnimal "monkey Ben 6" @?=
+        Right (Lib2.Animal {Lib2.species = "monkey", Lib2.name = "Ben", Lib2.age = 6}),
+
+    testCase "parseAnimal valid input with extra spaces" $
+      Lib2.parseAnimal "dog  Max   5  " @?=
+        Right (Lib2.Animal {Lib2.species = "dog", Lib2.name = "Max", Lib2.age = 5}),
+
+    testCase "parseAnimal invalid input" $
+      Lib2.parseAnimal "12345 Ben 6" @?=
+        Left "Expected string, but got none",
+
+
+    -- for parseString
+    testCase "parseString valid input" $
+      Lib2.parseString "Animal shelter" @?=
+        Right ("Animal", " shelter"),
+
+    testCase "parseString invalid input" $
+      Lib2.parseString "123abc" @?=
+        Left "Expected string, but got none",
+
+
+    -- for parseNumber
+    testCase "parseNumber valid input" $
+      Lib2.parseNumber "123" @?=
+        Right (123, ""),
+
+    testCase "parseNumber invalid input" $
+      Lib2.parseNumber "abc123" @?=
+        Left "Expected number, but got none",
+
+
+    -- -- for parseQuery     
+    testCase "parseQuery valid input for list_animals" $
+      Lib2.parseQuery "list_animals" @?=
+        Right Lib2.ListAnimals,
+
+    testCase "parseQuery valid input for ADD command" $
+      Lib2.parseQuery "ADD cat Tom 3" @?=
+        Right (Lib2.Add (Lib2.Animal "cat" "Tom" 3)),
+
+    testCase "parseQuery valid input for DELETE command" $
+      Lib2.parseQuery "DELETE dog Max 5" @?=
+        Right (Lib2.Delete (Lib2.Animal "dog" "Max" 5)),
+
+    testCase "parseQuery invalid command" $
+      Lib2.parseQuery "bad_command" @?=
+        Left "Did not get a valid command",
+
+    -- using CompoundQuery
+    testCase "parseQuery valid input for compound query" $
+      Lib2.parseQuery "ADD cat Tom 3; DELETE dog Max 5" @?=
+        Right (Lib2.CompoundQuery 
+                (Lib2.Add (Lib2.Animal "cat" "Tom" 3)) 
+                (Lib2.Delete (Lib2.Animal "dog" "Max" 5))),
+
+
+    -- for parseCompoundQuery
+    testCase "parseCompoundQuery valid input" $
+      Lib2.parseCompoundQuery "ADD cat Tom 3; DELETE dog Max 5" @?=
+        Right (Lib2.CompoundQuery 
+                (Lib2.Add (Lib2.Animal "cat" "Tom" 3)) 
+                (Lib2.Delete (Lib2.Animal "dog" "Max" 5))),
+
+    testCase "parseCompoundQuery valid input - nested compound query" $
+      Lib2.parseCompoundQuery "ADD cat Tom 3; ADD dog Max 5; DELETE dog Max 5; ADD fish Jem 1" @?=
+        Right (Lib2.CompoundQuery 
+                (Lib2.Add (Lib2.Animal "cat" "Tom" 3))
+                (Lib2.CompoundQuery 
+                  (Lib2.Add (Lib2.Animal "dog" "Max" 5))
+                  (Lib2.CompoundQuery 
+                    (Lib2.Delete (Lib2.Animal "dog" "Max" 5))
+                    (Lib2.Add (Lib2.Animal "fish" "Jem" 1))))),
+
+    testCase "parseCompoundQuery invalid input - empty second command" $
+      Lib2.parseCompoundQuery "ADD cat Tom 3;" @?=
+        Left "Expected some command but did not get anything",
+
+
+    -- for parseAdd
+    testCase "ADD valid" $
+      Lib2.parseAdd "ADD cat Luna 3" @?= 
+        Right (Lib2.Add (Lib2.Animal "cat" "Luna" 3)),
+
+    testCase "ADD invalid" $
+      Lib2.parseAdd "ADD Juppy 15" @?= 
+        Left "Expected string, but got none",
+
+
+    -- for parseDelete
+    testCase "DELETE valid" $
+      Lib2.parseDelete "DELETE dog Max 5" @?= 
+        Right (Lib2.Delete (Lib2.Animal "dog" "Max" 5)),
+
+    testCase "DELETE invalid" $
+      Lib2.parseDelete "DELETE dog 5" @?= 
+        Left "Expected string, but got none",
+
+
+    -- No need for 'State' test bc it is just data type
+
+
+    -- for emptyState and stateTransition
+    testCase "StateTransition: starts with emptyState, add animal" $
+      case Lib2.stateTransition Lib2.emptyState (Lib2.Add (Lib2.Animal "dog" "Max" 5)) of
+        Right (msg, Lib2.State animals) -> do
+          msg @?= Just "Added animal: Animal {species = \"dog\", name = \"Max\", age = 5}"
+          animals @?= [Lib2.Animal "dog" "Max" 5]
+        _ -> error "Test failed: adding an animal did not work as expected",
+
+    testCase "StateTransition: starts with not an emptyState, add animal" $
+      let initialState = Lib2.State [Lib2.Animal "cat" "Tom" 3]
+      in case Lib2.stateTransition initialState (Lib2.Add (Lib2.Animal "dog" "Max" 5)) of
+        Right (msg, Lib2.State animals) -> do
+          msg @?= Just "Added animal: Animal {species = \"dog\", name = \"Max\", age = 5}"
+          animals @?= [Lib2.Animal "dog" "Max" 5, Lib2.Animal "cat" "Tom" 3]
+        _ -> error "Test failed: adding an animal to a non-empty state did not work as expected",
+
+    testCase "StateTransition: add animal that already exists" $
+      let initialState = Lib2.State [Lib2.Animal "dog" "Max" 5]
+      in case Lib2.stateTransition initialState (Lib2.Add (Lib2.Animal "dog" "Max" 5)) of
+        Left err -> err @?= "Animal Animal {species = \"dog\", name = \"Max\", age = 5} already exists."
+        _ -> error "Test failed: should not add an animal that already exists",
+
+    testCase "StateTransition: delete animal from non-empty state" $
+      let initialState = Lib2.State [Lib2.Animal "dog" "Max" 5]
+      in case Lib2.stateTransition initialState (Lib2.Delete (Lib2.Animal "dog" "Max" 5)) of
+        Right (msg, Lib2.State animals) -> do
+          msg @?= Just "Deleted animal: Animal {species = \"dog\", name = \"Max\", age = 5}"
+          animals @?= []
+        _ -> error "Test failed: deleting an animal did not work as expected",
+
+    testCase "StateTransition: delete animal that does not exist" $
+      let initialState = Lib2.State [Lib2.Animal "cat" "Tom" 3]
+      in case Lib2.stateTransition initialState (Lib2.Delete (Lib2.Animal "dog" "Max" 5)) of
+        Left err -> err @?= "Animal Animal {species = \"dog\", name = \"Max\", age = 5} not found."
+        _ -> error "Test failed: should not delete an animal that does not exist",
+
+    testCase "StateTransition: list animals in empty state" $
+      case Lib2.stateTransition Lib2.emptyState Lib2.ListAnimals of
+        Right (msg, _) -> msg @?= Just "No animals found."
+        _ -> error "Test failed: listing animals in empty state did not work as expected",
+
+    testCase "StateTransition: list animals in non-empty state" $
+      let stateWithAnimals = Lib2.State [Lib2.Animal "dog" "Max" 5, Lib2.Animal "cat" "Tom" 3]
+      in case Lib2.stateTransition stateWithAnimals Lib2.ListAnimals of
+        Right (msg, _) -> msg @?= Just "Current animals: [Animal {species = \"dog\", name = \"Max\", age = 5},Animal {species = \"cat\", name = \"Tom\", age = 3}]"
+        _ -> error "Test failed: listing animals did not work as expected"
   ]
