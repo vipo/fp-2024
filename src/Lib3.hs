141099943a56faf27bc039a50db33a76caec0ebf
Ramunė Riaubaitė <ramune.riaubaite@mif.stud.vu.lt>
2024-11-10 13:21:30 +0000

    Implementing new Ininput types

--- src/Lib3.hs
+++ src/Lib3.hs
@@ -1,87 +1,101 @@
 {-# LANGUAGE InstanceSigs #-}
+
 module Lib3
     ( stateTransition,
-    StorageOp (..),
-    storageOpLoop,
-    parseCommand,
-    parseStatements,
-    marshallState,
-    renderStatements
+      StorageOp (..),
+      storageOpLoop,
+      parseCommand,
+      parseStatements,
+      marshallState,
+      renderStatements,
+      Statements(..),
+      Command(..),
     ) where
-
-import Control.Concurrent ( Chan )
-import Control.Concurrent.STM(STM, TVar)
-import qualified Lib2
 
 import System.IO (withFile, IOMode(..), hPutStr, hGetContents)
 import Control.Concurrent.Chan (Chan, readChan, writeChan)
 import Control.Exception (try, SomeException)
-
+import Control.Concurrent.STM (STM, TVar)
+import Data.Either (partitionEithers)
+import Control.Concurrent (Chan)
+import Data.List (isPrefixOf)
+import qualified Lib2
 
 
 data StorageOp = Save String (Chan ()) | Load (Chan String)
--- | This function is started from main
--- in a dedicated thread. It must be used to control
--- file access in a synchronized manner: read requests
--- from chan, do the IO operations needed and respond
--- to a channel provided in a request.
--- Modify as needed.
+
 storageOpLoop :: Chan StorageOp -> IO ()
 storageOpLoop chan = do
-  -- Read the next operation from the channel
   op <- readChan chan
   case op of
     Save content notifyChan -> do
-      -- Atytempt to write the content to a file
-      result <- try $ withFile "state.txt" WriteMode $ \handle -> hPutStr handle content -- 'withFile' for file access; for Save, we open in 'WriteMode'
+      result <- try $ withFile "state.txt" WriteMode $ \handle -> hPutStr handle content
       case result of
         Left err -> putStrLn $ "Error saving state: " ++ show (err :: SomeException)
-        Right _  -> writeChan notifyChan ()  -- Notify completion
-      storageOpLoop chan  -- Loop agai nfor next command
+        Right _  -> writeChan notifyChan ()
+      storageOpLoop chan
       
     Load notifyChan -> do
-      -- Attempt to read from the file
-      result <- try $ withFile "state.txt" ReadMode hGetContents -- for Load, we open it in 'ReadMode' and read all
+      result <- try $ withFile "state.txt" ReadMode hGetContents
       case result of
         Left err     -> writeChan notifyChan $ "Error loading state: " ++ show (err :: SomeException)
-        Right content -> writeChan notifyChan content  -- Send back the file contents
-      storageOpLoop chan  -- Loop again for next command
+        Right content -> writeChan notifyChan content
+      storageOpLoop chan
+
+
+-- DT that represents batch or single query (Add Animal, Delete Animal, ListAnimals, CompoundQuery Query Query)
+-- 'Statements' gives only 'Single' or 'Batch'
+data Statements = Batch [Lib2.Query] | Single Lib2.Query
+    deriving (Show, Eq)
+
+data Command = StatementCommand Statements | LoadCommand | SaveCommand
+    deriving (Show, Eq)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-data Statements = Batch [Lib2.Query] |
-               Single Lib2.Query
-               deriving (Show, Eq)
-
-data Command = StatementCommand Statements |
-               LoadCommand |
-               SaveCommand
-               deriving (Show, Eq)
-
--- | Parses user's input.
 parseCommand :: String -> Either String (Command, String)
-parseCommand _ = Left "Not implemented 2"
+parseCommand input
+  | "LOAD" `isPrefixOf` input = Right (LoadCommand, drop 4 input)
+  | "SAVE" `isPrefixOf` input = Right (SaveCommand, drop 4 input)
+  | otherwise = -- if input does not start w "LOAD" or "SAVE"
+      case parseStatements input of
+          Left err -> Left err
+          Right (gotStatements, rest) -> Right (StatementCommand gotStatements, rest)
 
--- | Parses Statement.
--- Must be used in parseCommand.
--- Reuse Lib2 as much as you can.
--- You can change Lib2.parseQuery signature if needed.
+-- | Parses Statement(s) from the input.
+-- Decides if it's a single query or a batch of queries.
 parseStatements :: String -> Either String (Statements, String)
-parseStatements _ = Left "Not implemented 3"
+parseStatements input = 
+    let queries = map (Lib2.parseQuery . trim) (splitOn ';' input) -- breaks down the input into individual commands and attempts to parse each one
+        (errors, parsedQueries) = partitionEithers queries
+    in case errors of
+         [] -> if length parsedQueries == 1
+                 then Right (Single (head parsedQueries), "")
+                 else Right (Batch parsedQueries, "")
+         _ -> Left "One or more queries in the batch could not be parsed."
+
+
+-- to trim whitespace
+trim :: String -> String
+trim = f . f -- applies f and applies f again on result
+   where f = reverse . dropWhile (== ' ') -- drops ' ', reverses, drops ' ', reverses again
+
+-- to split input
+splitOn :: Char -> String -> [String] -- Char - the delimiter
+splitOn _ [] = [""] -- base case
+splitOn delimiter (c:cs) -- check if the current character c is the delimiter
+    | c == delimiter = "" : rest -- when c is the delimiter, we start a new segment by adding an empty string at the beginning of rest
+    | otherwise = (c : head rest) : tail rest -- when c is not the delimiter, we add it to the start of the first segment in rest
+  where
+    rest = splitOn delimiter cs -- rest - the result of applying splitOn to the remaining characters cs
+
+
+
+
+
+
+
+
 
 -- | Converts program's state into Statements
 -- (probably a batch, but might be a single query)
@@ -110,3 +124,30 @@ renderStatements _ = error "Not implemented 5"
 stateTransition :: TVar Lib2.State -> Command -> Chan StorageOp ->
                    IO (Either String (Maybe String))
 stateTransition _ _ ioChan = return $ Left "Not implemented 6"
+
+
+
+-- for testing
+main :: IO ()
+main = do
+    -- Test for LoadCommand
+    print $ parseCommand "LOAD"          -- Expected: Right (LoadCommand, "")
+    
+    -- Test for SaveCommand
+    print $ parseCommand "SAVE"          -- Expected: Right (SaveCommand, "")
+    
+    -- Test for a single ADD command
+    print $ parseCommand "ADD dog Max 5" 
+    -- Right (StatementCommand (Single (Add (Animal {species = "dog", name = "Max", age = 5}))),"")
+    
+    -- Test for a single DELETE command
+    print $ parseCommand "DELETE cat Whiskers 3" 
+    -- Right (StatementCommand (Single (Delete (Animal {species = "cat", name = "Whiskers", age = 3}))),"")
+    
+    -- Test for a batch of commands (ADD + DELETE)
+    print $ parseCommand "ADD dog Max 5; DELETE cat Whiskers 3"
+    -- Right (StatementCommand (Batch [Add (Animal {species = "dog", name = "Max", age = 5}),Delete (Animal {species = "cat", name = "Whiskers", age = 3})]),"")
+    
+    -- Test for invalid command
+    print $ parseCommand "INVALID_COMMAND"
+    -- Left "One or more queries in the batch could not be parsed."
