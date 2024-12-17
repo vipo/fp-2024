2eb34c0464f038ce196c7ddd7902b2656d967c36
linasbau <144276662+linasbau@users.noreply.github.com>
2024-12-17 12:40:07 +0000

    One by one interpreter

--- app4-client/Main.hs
+++ app4-client/Main.hs
@@ -1,12 +1,82 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 module Main (main) where
 
 import Data.ByteString
 import Network.Wreq
 import Data.String.Conversions
-import Control.Lens
+import Control.Lens 
+import Control.Monad.Free (Free (..), liftF)
+
+data MyDomainAlgebra next
+  = View (String -> next)
+  | Load (String -> next)
+  | Save (String -> next)
+  | Delete (String -> next)
+  | Add String (String -> next) 
+  deriving (Functor)
+
+type DeckProgram = Free MyDomainAlgebra
+
+view :: DeckProgram String
+view = liftF $ View id
+
+load :: DeckProgram String
+load = liftF $ Load id
+
+save :: DeckProgram String
+save = liftF $ Save id
+
+delete :: DeckProgram String
+delete = liftF $ Delete id
+
+add :: String -> DeckProgram String
+add s = liftF $ Add s id
+
+
+
+oneByOne :: DeckProgram a -> IO (String, a)
+oneByOne = runStep ""
+  where
+    runStep acc (Pure a) = return (acc, a)
+    runStep acc (Free (View next)) = do
+      putStrLn "Sending request: view"
+      resp <- post "http://localhost:3000" (cs "view" :: ByteString)
+      let responseStr = cs $ resp ^. responseBody
+      runStep (acc ++ "View Response: " ++ responseStr ++ "\n") (next responseStr)
+    runStep acc (Free (Load next)) = do
+      putStrLn "Sending request: load"
+      resp <- post "http://localhost:3000" (cs "load" :: ByteString)
+      let responseStr = cs $ resp ^. responseBody
+      runStep (acc ++ "Load Response: " ++ responseStr ++ "\n") (next responseStr)
+    runStep acc (Free (Save next)) = do
+      putStrLn "Sending request: save"
+      resp <- post "http://localhost:3000" (cs "save" :: ByteString)
+      let responseStr = cs $ resp ^. responseBody
+      runStep (acc ++ "Load Response: " ++ responseStr ++ "\n") (next responseStr)
+    runStep acc (Free (Delete next)) = do
+      putStrLn "Sending request: delete"
+      resp <- post "http://localhost:3000" (cs "delete" :: ByteString)
+      let responseStr = cs $ resp ^. responseBody
+      runStep (acc ++ "Load Response: " ++ responseStr ++ "\n") (next responseStr)
+    runStep acc (Free (Add s next)) = do
+      putStrLn ("Adding: " ++ s)
+      resp <- post "http://localhost:3000" (cs ("add " ++ s) :: ByteString)
+      let responseStr = cs $ resp ^. responseBody
+      runStep (acc ++ "Add Response: " ++ responseStr ++ "\n") (next responseStr)
+
+
+program :: DeckProgram String
+program = do
+    Main.delete
+    load
+    add "Two of Spades, Joker"
+    Main.view
+    
+
 
 main :: IO ()
 main = do
-    let rawRequest = cs "works?" :: ByteString
-    resp <- post "http://localhost:3000" rawRequest
-    putStrLn $ cs $ resp ^. responseBody
+    (responses, _) <- oneByOne program
+    putStrLn "Accumulated Responses:"
+    putStrLn responses
--- app4-server/Main.hs
+++ app4-server/Main.hs
@@ -1,13 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Control.Monad.IO.Class(liftIO)
-import Data.String.Conversions
+import Control.Concurrent (forkIO)
+import Control.Concurrent.Chan (Chan, newChan)
+import Control.Concurrent.STM (TVar, newTVarIO)
+import Control.Monad.IO.Class (liftIO)
+import Data.Maybe (fromMaybe)
+import Data.String.Conversions (cs)
+import qualified Lib2
+import qualified Lib3
 import Web.Scotty
 
 main :: IO ()
-main = scotty 3000 $
+main = do
+  chan <- newChan :: IO (Chan Lib3.StorageOp)
+  state <- newTVarIO Lib2.emptyState
+  _ <- forkIO $ Lib3.storageOpLoop chan
+  scotty 3000 $
     post "/" $ do
-        b <- body
-        liftIO $ putStrLn $ concat ["Request was: ", cs b]
-        text $ "This is response"
+      b <- body
+      liftIO $ putStrLn ("Request was: " ++ cs b)
+      response <- liftIO $ proccess state chan $ cs b
+      text $ cs response
+
+proccess :: TVar Lib2.State -> Chan Lib3.StorageOp -> String -> IO String
+proccess state storageChan input = case Lib3.parseCommand input of
+  Left e -> return e
+  Right (cmd, "") -> do
+    info <- Lib3.stateTransition state cmd storageChan
+    case info of
+      Left e -> return e
+      Right mb -> return $ fromMaybe "Success" mb
+  Right (_, str) -> return $ "Could not parse: " ++ str
